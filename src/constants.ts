f9849e989e239bb80a1d18aa287619110b5739ee
Harry Stuart Curtis <harrycurtis@live.com>
2024-10-23 17:05:59 +0100

    feat: added constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,8 @@
-// The income threshold to meet the UK Family Visa financial requirement as of April 2024.
+// As of April 2024, the income threshold to meet the UK Family Visa financial requirement is £29,000.
 export const CURRENT_INCOME_THRESHOLD: number = 29000;
+
+// As of April 2024, the lower savings threshold for savings to count towards the financial requirement is £16,000.
+export const CURRENT_SAVINGS_THRESHOLD: number = 16000;
+
+// As of April 2024, the minimum number of years required to sustain the applicant is 2.5, when used as a multiple for the weight of the savings towards meeting the financial requirement.
+export const CURRENT_MIN_YEARS: number = 2.5;
