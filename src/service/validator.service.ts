157d8a4441c16e7f4570055cb613b9c7282f7270
Harry Stuart Curtis <harrycurtis@live.com>
2024-10-24 10:58:44 +0100

    feat: Validator service class

--- src/service/validator.service.ts
+++ src/service/validator.service.ts
@@ -1,7 +1,13 @@
 export class Validator {
-    static isPositiveNum(value: number) {
-        if (value < 0) {
-            throw new Error("value < 0");
-        }
+  static isPositiveNum(value: number) {
+    if (value < 0) {
+      throw new Error("value < 0");
     }
+  }
+
+  static isNumber(value: unknown) {
+    if (typeof value !== "number") {
+      throw new Error('typeof value !== "number"');
+    }
+  }
 }
--- test/service/validator.service.test.ts
+++ test/service/validator.service.test.ts
@@ -1,18 +1,66 @@
 import { describe, it } from "jsr:@std/testing/bdd";
-import { assertThrows } from "jsr:@std/assert";
+import { expect } from "jsr:@std/expect";
 import { Validator } from "../../src/service/validator.service.ts";
 
 describe("Validator Service tests", () => {
-  it("throws an error if the received value is a number less than 0", () => {
-    // Assemble
-    // Set three randomly chosen values for values less than 0.
-    const testValues: number[] = [-13657, -78254, -35101];
+  describe("isPositiveNum tests", () => {
+    it("throws an error if the received value is a number less than 0", () => {
+      // Assemble
+      // Set three randomly chosen values for values less than 0.
+      const testValues: number[] = [-13657, -78254, -35101];
 
-    // Act & Assert
-    // Check that each negative value causes an error to be thrown.
-    testValues.forEach((value) => {
-      assertThrows(() => {
-        Validator.isPositiveNum(value);
+      // Act & Assert
+      // Check that each negative value causes an error to be thrown.
+      testValues.forEach((value) => {
+        expect(() => {
+          Validator.isPositiveNum(value);
+        }).toThrow();
+      });
+    });
+
+    it("does not throw an error if the received value is a number equal to or greater than 0", () => {
+      // Assemble
+      // Set three randomly chosen values for values greater than or equal to 0.
+      const testValues: number[] = [0, 20543, 23713];
+
+      // Act & Assert
+      // Check that each negative value does not cause an error to be thrown.
+      testValues.forEach((value) => {
+        expect(() => {
+          Validator.isPositiveNum(value);
+        }).not.toThrow();
+      });
+    });
+  });
+
+  describe("isNumber tests", () => {
+    it("throws an error if the received value is not a number", () => {
+      // Assemble
+      // Set three values that are not numbers.
+      const testValues = ["Hello, World!", [1, 2, 3], true];
+
+      // Act & Assert
+      // For all test values...
+      testValues.forEach((value) => {
+        expect(() => {
+          // ...Check that each value causes an exception to be thrown.
+          Validator.isNumber(value);
+        }).toThrow();
+      });
+    });
+
+    it("does not throw an error if the received value is a number", () => {
+      // Assemble
+      // Set three values that are numbers.
+      const testValues = [8073, -7762, 29938.8];
+
+      // Act & Assert
+      // For all test values...
+      testValues.forEach((value) => {
+        expect(() => {
+          // ...Check that each value does not cause an exception to be thrown.
+          Validator.isNumber(value);
+        }).not.toThrow();
       });
     });
   });
