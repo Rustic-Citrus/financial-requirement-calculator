bacaa7c763e4e50005d654171fe34563513a715f
Harry Stuart Curtis <harrycurtis@live.com>
2024-10-24 11:44:25 +0100

    docs: added comments

--- src/service/formatter.service.ts
+++ src/service/formatter.service.ts
@@ -1,5 +1,6 @@
 export class Formatter {
   static buildPoundStr(value: number): string {
+    // Return a string of the number formatted to 2 decimal places and affixed with a pound symbol.
     return value.toLocaleString("en-GB", {
       style: "currency",
       currency: "GBP",
--- src/service/validator.service.ts
+++ src/service/validator.service.ts
@@ -1,11 +1,13 @@
 export class Validator {
   static isPositiveNum(value: number) {
+    // Throw an exception if the value is less than 0.
     if (value < 0) {
       throw new Error("value < 0");
     }
   }
 
   static isNumber(value: unknown) {
+    // Throw an exception if the value is not a number.
     if (typeof value !== "number") {
       throw new Error('typeof value !== "number"');
     }
