4960e0efd42e69c59067d8026a796b44534d325b
Harry Stuart Curtis <harrycurtis@live.com>
2024-10-29 10:32:52 +0000

    test: Controller coverage

--- test/controller/controller.test.ts
+++ test/controller/controller.test.ts
@@ -1,6 +1,8 @@
 import { beforeEach, describe, it } from "@std/testing/bdd";
+import { stub } from "@std/testing/mock";
 import { expect } from "jsr:@std/expect/expect";
 import { Controller } from "../../src/controller/index.ts";
+import { Validator } from "../../src/util/validator.util.ts";
 
 describe("Controller tests", () => {
   let testController: Controller;
@@ -83,6 +85,37 @@ describe("Controller tests", () => {
       expect(response.status).toEqual(200);
       expect(await response.text()).toEqual("£24,400.00");        
     });
+
+    it("returns a 400-response when a non-error is thrown", () => {
+      // Setup
+      // Save a reference to the original implementation of isNumber.
+      const originalIsNumber = Validator.isNumber
+
+      // Arrange
+      // Create a stub of a Validator method which throws something other than an Error.
+      stub(Validator, "isNumber", () => {
+        throw "Woops!";
+      });
+
+      // Create a mock URL.
+      mockUrl = "http://localhost:4000/?savings=27500";
+
+      // Create a mock Request.
+      const mockRequest = new Request(mockUrl, {
+        method: "GET",
+      });
+
+      // Act
+      // Try to process a perfectly valid request.
+      const response = testController.handleGetIncome(mockRequest);
+
+      // Assert
+      expect(response.status).toEqual(400);
+      expect(response.statusText).toEqual("Woops!");
+
+      // Teardown
+      Validator.isNumber = originalIsNumber;
+    })
   });
 
   describe("handleGetSavings tests", () => {
