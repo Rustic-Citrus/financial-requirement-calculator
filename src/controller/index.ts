87793c356949dd1df907be9bbcccc13102216e5c
Harry Stuart Curtis <harrycurtis@live.com>
2024-11-05 11:48:57 +0000

    refactor: Controller class

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -1,5 +1,6 @@
 import { Calculator } from "../service/index.ts";
-import { Validator, Formatter } from "../util/index.ts";
+import { Validator } from "../util/index.ts";
+import { Builder } from "../util/builder.util.ts";
 import {
   CURRENT_INCOME_THRESHOLD,
   CURRENT_MIN_YEARS,
@@ -17,42 +18,14 @@ export class Controller {
     );
   }
 
-  getParams(req: Request): URLSearchParams {
-    // Retrieve the search parameters from the request object.
-    return new URLSearchParams(new URL(req.url).search);
-  }
-
-  getResponse(
-    paramNumber: number,
-    expectedParam: "income" | "savings"
-  ): Response {
-    // Calculate the required income from the savings or the required savings from the income, format the output, and return the response to user.
-    return expectedParam === "income"
-      ? new Response(
-          Formatter.buildPoundStr(
-            this.calculatorService.calculateSavings(paramNumber)
-          )
-        )
-      : new Response(
-          Formatter.buildPoundStr(
-            this.calculatorService.calculateIncome(paramNumber)
-          )
-        );
-  }
-
-  handleThrowable(error: unknown): Response {
-    // Return the formatted response with error message or error as string.
-    return new Response(null, {
-      status: 400,
-      statusText: error instanceof Error ? error.message : String(error),
-    });
-  }
-
   handleGet(req: Request, expectedParam: "income" | "savings"): Response {
     try {
+      // 1. VALIDATION STEPS
+
       // Retrieve the search parameters from the request object, check that the URL has the expected parameter, then save the value if it does.
-      Validator.hasParam(this.getParams(req), expectedParam);
-      const paramValue = this.getParams(req).get(expectedParam);
+      const params = Builder.buildParamsFromRequest(req);
+      Validator.hasParam(params, expectedParam);
+      const paramValue = params.get(expectedParam);
 
       // @ts-ignore Parameter existence checked. Check whether the parameter value is a number.
       Validator.isNumber(Number.parseFloat(paramValue));
@@ -63,11 +36,17 @@ export class Controller {
       // @ts-ignore Parameter value is definitely a positive number. Save value as a number to variable.
       const paramNumber: number = Number.parseFloat(paramValue);
 
+      // 2. CALCULATION AND OUTPUT STEP
+
       // Build the appropriate response.
-      return this.getResponse(paramNumber, expectedParam);
+      return Builder.buildResponseFromCalculation(
+        paramNumber,
+        expectedParam,
+        this.calculatorService
+      );
     } catch (error: unknown) {
       // Handle any errors.
-      return this.handleThrowable(error);
+      return Builder.buildResponseFromThrowable(error);
     }
   }
 }
